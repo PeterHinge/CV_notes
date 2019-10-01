9d7e900c2b8706d4e50a8159f3a07846b57db5e5
unknown <contactpeterhinge@gmail.com>
2019-10-01 19:13:51 +0200

    testing

--- opencv.py
+++ opencv.py
@@ -8,7 +8,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
-while 1:
+while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
